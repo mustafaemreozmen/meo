04b1fadd0c501808fe064f498f9f3b762317d484
A. Goktug Yalcin <goktugyalcin06@gmail.com>
2023-04-17 13:47:54 +0300

    Development

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 #! /usr/bin/env node
 const tk = require("terminal-kit").terminal;
 const resume = require("./resume.json");
-const { slowTyping, drawImage, singleColumnMenu } = require("./wrappers");
+const { ResumeTerminal } = require("./ResumeTerminal");
 
-async function render() {
-  tk.wrapColumn({ x: 10, width: 25 });
-  tk.on("key", (key) => {
-    if (key == "CTRL_C" || key == "ESC") {
-      tk.grabInput(false);
-      tk.nextLine;
-      process.exit(0);
-    }
-  });
+const resumeTerminal = new ResumeTerminal(tk, resume);
 
-  await drawImage(tk, resume);
-  await slowTyping(tk, "\nHello! My name is ", "magenta");
-  await slowTyping(tk, `${resume.name} ${resume.surname}\n`, "white");
-  tk.brightBlue("I'm a ")
-    .bold.yellow(`${resume.workExperience.at(0)?.title} `)
-    .brightBlue("from ")
-    .bold.red(`${resume.links.find((x) => x.type == "ADDRESS")?.value}\n\n`);
-  await slowTyping(tk, "Choose for more detail:");
-  singleColumnMenu(tk, resume);
-}
-
-render();
+resumeTerminal.render();
